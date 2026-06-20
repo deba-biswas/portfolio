c5345fe85d33b7c030e55b028a984de95fa42acf
Debangshu Biswas <debangshubiswas637@gmail.com>
2026-06-20 21:53:51 +0530

    Update resume link

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -67,7 +67,7 @@ const HeroSection = () => {
               Contact Me
             </a>
             <a
-              href="https://drive.google.com/uc?export=download&id=14iUAYT23LgUY6GBAywZzkMlZkvz-ABgj"
+              href="https://drive.google.com/uc?export=download&id=1RcW-qbaM3DJZ0iREiJOxY4jDL-XPsVzv"
               target="_blank"
               className="glass-card px-6 py-3 rounded-lg font-medium flex items-center gap-2 hover:bg-secondary transition-colors"
             >
